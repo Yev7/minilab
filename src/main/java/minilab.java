740d63617ffec0c0028c10beb1f0d1e6e2f6c6ed
Yevgeny Carranza <lijiu001@gmail.com>
2022-03-16 13:55:51 -0400

    Initial commit

---
+++ src/main/java/minilab.java
@@ -0,0 +1,38 @@
+public class minilab {
+    public static void main(String[] args) {
+        if (5>2) {
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+
+        int number = 24;
+
+        String result = (number > 0) ? "Positive Number" : "Negative Number";
+        System.out.println(result);
+
+        int color = 5;
+        switch (color) {
+            case 1:
+                System.out.println("Red");
+                break;
+            case 2:
+                System.out.println("Blue");
+                break;
+            case 3:
+                System.out.println("Green");
+                break;
+            case 4:
+                System.out.println("Yellow");
+                break;
+            case 5:
+                System.out.println("Orange");
+                break;
+            case 6:
+                System.out.println("Purple");
+                break;
+        }
+
+    }
+}
